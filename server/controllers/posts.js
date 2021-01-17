6d7139ea61a3d689b52a59f407a8858367f590da
fergus90 <f3r852@gmail.com>
2021-01-17 16:58:43 -0700

    01/17/2021

---
+++ server/controllers/posts.js
@@ -0,0 +1,7 @@
+export const getPosts = (req, res) => {
+  res.send('Works Fine');
+};
+
+export const createPost = (req, res) => {
+  res.send('Post Creation');
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 const ConnectionUrl =
-  "mongodb+srv://fergus1990:21428220@cluster0.fcim7.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  'mongodb+srv://fergus1990:21428220@cluster0.fcim7.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(ConnectionUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
