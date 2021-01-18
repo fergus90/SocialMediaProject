ada637445f421944b2109e8b09e4d6d6584674db
fergus90 <f3r852@gmail.com>
2021-01-17 17:26:59 -0700

    error fixed

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,9 @@
 import express from 'express';
 
-import { getPosts } from '../controllers/posts.js';
-import { createPost } from '../controllers/posts.js';
-
 const router = express.Router();
 
-router.get('/', getPosts);
-router.get('/', createPost);
+router.get('/', (req, res) => {
+  res.send('Works Fine!');
+});
 
 export default router;
