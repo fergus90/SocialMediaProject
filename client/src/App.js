fc182695c53995c69204991b4b93b8d0732d5455
fergus90 <f3r852@gmail.com>
2021-01-20 23:08:06 -0700

    01/20/2021

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import untitle from './images/moment.png';
+import untitle from './images/memories.png';
+import useStyles from './style';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center" style={{ fontWeight: 600 }}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography
+          className={classes.heading}
+          variant="h2"
+          align="center"
+          style={{ fontWeight: 600 }}
+        >
           Untitle Project
         </Typography>
-        <img src={untitle} alt="untitle" height="500" />
+        <img
+          className={classes.image}
+          src={untitle}
+          alt="untitle"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
