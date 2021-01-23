9d4362cfb6bc5bc38a46b1b29e6dc738d92f5068
fergus90 <f3r852@gmail.com>
2021-01-22 22:13:05 -0700

    01/22/2021

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,17 +2,27 @@ import React from 'react';
 import Post from './Post/Post';
 import useStyles from './style';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
   console.log(posts);
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post.id} item xs={12}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 export default Posts;
